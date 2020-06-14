a735871ffe86662997c141b0ed8a231bc3840a20
Mahmoud Ali <muddibr@gmail.com>
2020-06-14 12:56:17 -0300

    WIP

---
+++ src/TestSmells/Delay.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+namespace TestSmells
+{
+    public interface IDependencia
+    {
+        Task Chamada();
+    }
+
+    public class Dependencia : IDependencia
+    {
+        public async Task Chamada()
+        {
+            await Task.Yield();
+        }
+    }
+    
+    public class Delay
+    {
+        private readonly IDependencia _dependencia;
+        public bool EstáCarregando { get; set; }
+        
+        public Delay(IDependencia dependencia)
+        {
+            _dependencia = dependencia;
+        }
+        
+        public async Task FazerChamada()
+        {
+            EstáCarregando = true;
+
+            await _dependencia.Chamada();
+            await Task.Delay(5000);
+        }
+    }
+}
---
+++ test/TestSmells.UnitTests/Specs/DelayTestes.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace TestSmells.UnitTests.Specs
+{
+    public class DelayTestes
+    {
+        [Test]
+        public async Task FazerChamadaDeveMudarEstáCarregandoParaTrue()
+        {
+            var delay = new Delay(new Dependencia());
+
+            await delay.FazerChamada();
+
+            delay.EstáCarregando.Should().BeTrue();
+        }
+    }
+}
